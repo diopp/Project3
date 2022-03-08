ebd24710dd849c20c44867af5e142e7e64539741
diopp <49011476+diopp@users.noreply.github.com>
2022-03-08 05:49:09 -0800

    Changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 //Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 const { PORT = 3001,DATABASE_URL } = process.env
 require('dotenv').config()
@@ -13,10 +15,75 @@ mongoose.connection
 .on('close', () => console.log('You are disconnected to Mongodb'))
 .on('error', (error) => console.log('MongoDB error'));
 
+//Middlewares
+app.use(cors({
+    origin: "*",
+})
+);
+app.use(morgan('dev'))
+app.use(express.json())
+
+
+
+
+//Models 
+
+const ProductSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    image: String,
+    quantity: Number,
+    category: String,
+    pricelb: Number
+
+})
+const Product = mongoose.model('Product', ProductSchema)
+
+//Test Route
+app.get('/',(req,res) => {
+    res.send('Test')
+})
+
+//Product Index
+app.get('/product', async(req,res) => {
+    try{
+
+    res.json(await Product.find({}))
+    } catch(error){
+        res.status(400).json(error)
+    }
+
+})
+
+app.post('/product',async(req,res) => {
+    try{
+        res.json(await Product.create(req.body))
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+//Products DELETE ROUTE
+app.delete('product/:id', async (req,res) => {
+    try {
+        res.json(await Product.findByIdAndDelete(req.params.id));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
+// PRODUCT UPDATE ROUTE
+app.put('product/:id', async (req,res) => {
+    try {
+        res.json(await Product.findByIdAndUpdate(req.params.id, req.body));
+    } catch (error) {
+        res.status(400).json(error); 
+    }
+})
+
 
 //Middlewares 
-const cors = require('cors');
-const morgam = require('morgan');
+
 
 
 
